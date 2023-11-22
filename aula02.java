b83ab28c8d7ee6ad5acb82ffd1ee8c392f08211a
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2023-11-22 08:19:54 -0300

    Aula02 - arquivo que possui o MAIN

---
+++ aula02.java
@@ -0,0 +1,11 @@
+public class aula02 {
+   public static void main(String[] args){
+        caneta c1 = new caneta();
+        c1.setModelo("bic");
+        c1.setPonta(0.5f);
+        c1.status();
+
+        
+
+    }
+}
