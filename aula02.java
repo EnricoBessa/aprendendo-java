facc9c88fbf882a25c6ef63b7de2351d86b61030
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2023-11-23 14:43:47 -0300

    Adicionando linhas de código

--- aula02.java
+++ aula02.java
@@ -1,11 +1,26 @@
 public class aula02 {
    public static void main(String[] args){
+        //caneta
         caneta c1 = new caneta();
         c1.setModelo("bic");
         c1.setPonta(0.5f);
         c1.status();
-
+        System.out.println("----------------------------");
         
+        //conta banco
+        contabanco p1 = new contabanco();
+        p1.setDono("Armindo");
+        p1.setNumConta(1000);
+        p1.abrirConta("cc");
+        p1.depositar(100);
+        contabanco p2 = new contabanco();
+        p2.setDono("Robert");
+        p2.setNumConta(1001);
+        p2.abrirConta("cp");
+        p2.depositar(500);
+        p2.sacar(180);
 
+        p1.estado();
+        p2.estado();
     }
 }
--- contabanco.java
+++ contabanco.java
@@ -6,58 +6,115 @@ public class contabanco {
     private float saldo;
     private boolean status;
 
-    public void abirConta(String t){
+    public void estado(){
+        System.out.println("----------------------------");
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Conta: " + this.getNumConta())
+        ;System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Status: " + this.getStatus());
 
     }
-    public void fecharConta(){
-        
-    }
-    public void depositar(){
-        
-    }
-    public void sacar(){
-        
-    }
-    public void pagarMensal(){
-        
+
+    public void abrirConta(String t) {
+        this.setTipo(t);
+        this.setStatus(true);
+        if (t == "cc") {
+            this.setSaldo(50);
+        } else if (t == "cp") {
+            this.setSaldo(150);
+        }
     }
 
-    public contabanco(){
-        
+    public void fecharConta() {
+        if (this.getSaldo() > 0) {
+            System.out.println("Conta com dinheiro");
+        } else if (this.getSaldo() < 0) {
+            System.out.println("Conta em debito");
+        } else {
+            setStatus(false);
+            System.out.println("Conta fechada");
+        }
     }
 
-    public void setNumConta(int n){
+    public void depositar(float v) {
+        if (this.getStatus()) {
+            this.setSaldo((this.getSaldo() + v));
+            System.out.println("Dposito realizado para o " + this.getDono());
+        } else {
+            System.out.println("Imposivel depositar");
+        }
+    }
+
+    public void sacar(float v) {
+        if (this.getStatus()) {
+            if (this.getSaldo() >= v) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta de " + getDono());
+            } else {
+                System.out.println("Saldo insuficiente");
+            }
+        } else {
+            System.out.println("Impossivel sacar de uma conta fechada");
+        }
+    }
+
+    public void pagarMensal() {
+        int v = 0;
+        if (this.getTipo() == "cc") {
+            v = 12;
+        } else if (this.getTipo() == "cp") {
+            v = 20;
+        }
+        if (this.getStatus()) {
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mesalidade paga por " + getDono());
+        } else {
+            System.out.println("Impossivel pagar de uma conta fechada");
+        }
+    }
+
+    public contabanco() {
+        // Implementação do construtor, se necessário
+    }
+
+    public void setNumConta(int n) {
         this.numConta = n;
     }
-    public int getNumConta(int n){
+
+    public int getNumConta() {
         return this.numConta;
     }
 
-    public void setTipo(String t){
+    public void setTipo(String t) {
         this.tipo = t;
     }
-    public String getTipo(int t){
+
+    public String getTipo() {
         return this.tipo;
     }
 
-    public void setDono(int d){
-        this.numConta = d;
+    public void setDono(String d) {
+        this.dono = d;
     }
-    public String getDono(int n){
+
+    public String getDono() {
         return this.dono;
     }
 
-    public void setStatus(boolean st){
+    public void setStatus(boolean st) {
         this.status = st;
     }
-    public boolean getStatus(int st){
+
+    public boolean getStatus() {
         return this.status;
     }
 
-    public void setSaldo(int s){
+    public void setSaldo(float s) {
         this.saldo = s;
     }
-    public float getSaldo(int s){
+
+    public float getSaldo() {
         return this.saldo;
     }
 }
