27a36dabed0fa7427837a44e3ecc5b20b600b52a
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2023-11-28 19:16:19 -0300

    Aula06 - arquivo que possui o MAIN

---
+++ aula06.java
@@ -0,0 +1,11 @@
+public class aula06 {
+    public static void main(String[] args) {
+        controleremoto c = new controleremoto();
+        c.ligar();
+        c.maisVolume();
+        //c.ligarMudo();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+    }
+}
