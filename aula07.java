1086a7dea9f5c3c9c6d90c45252b88fc35c9df31
Enrico Bessa <86358593+EnricoBessa@users.noreply.github.com>
2023-12-11 15:43:41 -0300

    Aula07 - arquivo que possui o MAIN
    Alterações no main para rodar as novas operações

--- aula07.java
+++ aula07.java
@@ -2,12 +2,13 @@ public class aula07 {
     public static void main(String[] args) {
     lutador L[] = new lutador[4];
     L[0] = new lutador("Edinaldo", "Brasileiro", 27, 180, 80, 20, 10, 0);
-    L[1] = new lutador("Roger", "Brasileiro", 25, 185, 90, 16, 12, 8);
-    L[2] = new lutador("Chico", "Brasileiro", 30, 178, 70, 10, 8, 5);
-    L[3] = new lutador("Caze", "Brasileiro", 29, 189, 100, 0, 0, 30);    
+    L[1] = new lutador("Roger", "Brasileiro", 25, 185, 80, 16, 12, 8);
+    L[2] = new lutador("Chico", "Brasileiro", 30, 178, 90, 10, 8, 5);
+    L[3] = new lutador("Caze", "Brasileiro", 29, 189, 90, 0, 0, 30);    
 
-    L[3].apresentar();
-    L[3].status();
+    luta UFC01 = new luta();
+    UFC01.marcarLuta(L[0], L[1]);
+    UFC01.lutar();
     
 }   
 }
